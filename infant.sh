0cf7117e2bb048ae518755b1bc77dba017f19068
hjdong <1901919946@qq.com>
2019-01-22 22:08:10 +0800

    Update infant.sh

--- infant.sh
+++ infant.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#Ueage: sh infant.sh /Project/Data_20190118 20190118
-for i in 015-081-006 016-043-020
+#Ueage: sh infant.sh /Project/Data_20190118 infantName.list 20190118
+
+results_dir=$1
+infantID=$2
+project_date=$3
+
+for i in `cat $infantID|cut -f1`
 do
-cat $1/$i/result/Abundance.txt >> Abundance_infant_v6_$2.txt
-cat $1/$i/result/Diversity.txt >> Diversity_infant_v6_$2.txt
-cat $1/$i/result/Maturity.txt >> Maturity_infant_v6_$2.txt
-cat $1/$i/result/Vitamin.txt >> Vitamin_infant_v6_$2.txt
-cat $1/$i/result/Metabolizepathway.txt >> Metabolizepathway_infant_v6_$2.txt
+cat $results_dir/$i/result/Abundance.txt >> Abundance_infant_v6_$project_date.txt
+cat $results_dir/$i/result/Diversity.txt >> Diversity_infant_v6_$project_date.txt
+cat $results_dir/$i/result/Maturity.txt >> Maturity_infant_v6_$project_date.txt
+cat $results_dir/$i/result/Vitamin.txt >> Vitamin_infant_v6_$project_date.txt
+cat $results_dir/$i/result/Metabolizepathway.txt >> Metabolizepathway_infant_v6_$project_date.txt
 done
-tar zcvf infant.$2.tar.gz *.txt
+tar zcvf infant.$project_date.tar.gz *_infant*
