15b5634e21df654aff5f9a27f1f1b79ae2827931
hjdong <1901919946@qq.com>
2019-01-22 22:09:40 +0800

    Update mvFile.sh

--- mvFile.sh
+++ mvFile.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-#Ueage: sh mvFile.sh
-for i in 015-081-006 016-043-020 015-117-075
+#Usage: sh mvFile.sh /home/Project/Data_20190102/results sampleName.txt
+
+results_dir=$1
+kidID=$2
+for i in `cat $kidID|cut -f1`
 do
-mkdir $i
-mkdir $i/result
-mv *_${i}_* $i
-cp $i/*.apidir/*.txt $i/result
-cp $i/*maturity* $i/result/Maturity.txt
-cp $i/*vitamin* $i/result/Vitamin.txt
-cp $i/*.age_* $i/result/GutAge.txt
-cp $i/*bmi* $i/result/Forecasbmi.txt
-cp $i/*mdi* $i/result/MDI.txt
-cp $i/*skin* $i/result/AgeDifference.txt
-sed -i "s/\t/${i}\t/4" $i/result/AgeDifference.txt
-sed -i "s/Testsampleid/$i/" $i/result/*.txt
-sed -i "s/\t/${i}\t/4" $i/result/MDI.txt
-sed -i "s/nokiding/$i/" $i/result/*.txt
+mkdir $results_dir/$i
+mkdir $results_dir/$i/result
+mv $results_dir/*${i}_* $results_dir/$i
+cp $results_dir/$i/*.apidir/*.txt $results_dir/$i/result
+cp $results_dir/$i/*maturity* $results_dir/$i/result/Maturity.txt
+cp $results_dir/$i/*vitamin* $results_dir/$i/result/Vitamin.txt
+cp $results_dir/$i/*.age_* $results_dir/$i/result/GutAge.txt
+cp $results_dir/$i/*bmi* $results_dir/$i/result/Forecasbmi.txt
+cp $results_dir/$i/*mdi* $results_dir/$i/result/MDI.txt
+cp $results_dir/$i/*skin* $results_dir/$i/result/AgeDifference.txt
+sed -i "s/\t/${i}\t/4" $results_dir/$i/result/AgeDifference.txt
+sed -i "s/Testsampleid/$i/" $results_dir/$i/result/*.txt
+sed -i "s/\t/${i}\t/4" $results_dir/$i/result/MDI.txt
+sed -i "s/nokiding/$i/" $results_dir/$i/result/*.txt
 done
