0b8944df5ca9c6811f0a358262970bb662cc79b1
hjdong <1901919946@qq.com>
2019-03-04 13:03:20 +0800

    Update splitFile.sh

--- splitFile.sh
+++ splitFile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#从后台系统导出样品信息表，转为文本文件格式。
+#从后台系统导出样品信息表，保留第1、2、3、25列，转为文本文件格式。
 #用于从下机数据中挑选出需要交付报告的数据，存入delivery_data目录内
 
 #Usage:sh splitFile.sh /home/hjd/mnt/hrk20190118/rawdata hcode2019119.txt
@@ -7,11 +7,11 @@
 data_dir=$1  # 下机数据所在目录
 sample_list=$2 #样本信息表，文本格式
 
-cut -f1,2,25 $sample_list |grep "报告" > sample_delivery.txt
+cut -f1-4 $sample_list |grep "报告" > sample_delivery.txt
 
-grep "婴幼儿健康管家" sample_delivery.txt > baby_gut.txt
-grep "微生态健康管家" sample_delivery.txt > adult_gut.txt
-grep "肌本胚壤|微生态皮肤检测" sample_delivery.txt > skin.txt
+grep "婴幼儿肠道样本" sample_delivery.txt > baby_gut.txt
+grep -v -E "婴幼儿肠道样本|肌本胚壤" sample_delivery.txt > adult_gut.txt
+grep "肌本胚壤" sample_delivery.txt > skin.txt
 
 #统计个数
 FN=$(wc -l < sample_delivery.txt)
